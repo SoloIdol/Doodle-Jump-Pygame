67383eff8cd1b3e819201e0105415ffceed9f73a
universal developer <69350489+universal-developer@users.noreply.github.com>
2024-12-12 19:35:42 +0100

    🙋🏻‍♂️Better code structure

--- Doodlejump/DoodlejumpV2.py
+++ Doodlejump/DoodlejumpV2.py
@@ -1,64 +1,45 @@
-# V2
-
-
-# -----------------Libs----------------------------------------------------------------------------------------------------------------------------
-
 import pygame
-
 import random
 
-# -----------------Initializer----------------------------------------------------------------------------------------------------------------------
-
+# Initialisation de Pygame
 pygame.init()
 
-# -----------------Window---------------------------------------------------------------------------------------------------------------------------
-
+# Définition des dimensions de la fenêtre
 SCREEN_WIDTH = 514
 SCREEN_HEIGHT = 912
-
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Doodle Jump')
 
+# Configuration de l'horloge et des FPS
 clock = pygame.time.Clock()
 FPS = 60
 
-# -----------------Colors---------------------------------------------------------------------------------------------------------------------------
-
+# Définition des couleurs
 WHITE = (255, 255, 255)
 
-# -----------------Fonts---------------------------------------------------------------------------------------------------------------------------
-
+# Définition des polices de caractères
 font_small = pygame.font.SysFont('Lucida Sans', 20)
-
 font_big = pygame.font.SysFont('Lucida Sans', 25)
 
-# -----------------Game Vars-------------------------------------------------------------------------------------------------------------------------
-
+# Variables du jeu
 GRAVITY = 1
-
 SCROLL_THRESH = 200
-
 scroll = 0
-
 MAX_PLATFORMS = 10
-
 bg_scroll = 0
-
 game_over = False
-
 score = 0
-
 cycle = 1
-
 score2 = 0
 
+# Lecture du meilleur score depuis un fichier
 try:
     with open("topscore.txt", "r") as score_file:
         high_score = int(score_file.read())
 except (FileNotFoundError, ValueError):
     high_score = 0
-# -----------------Images---------------------------------------------------------------------------------------------------------------------------
 
+# Chargement des images
 bg_image = pygame.image.load('assets/StartCityBg.jpg').convert_alpha()
 bg2_image = pygame.image.load('assets/ScrollCityBg1.jpg').convert_alpha()
 bg3_image = pygame.image.load('assets/ScrollCityBg2.jpg').convert_alpha()
@@ -68,24 +49,23 @@ platform_green_image = pygame.image.load(
 platform_neon_image = pygame.image.load(
     'assets/neonplatform.png').convert_alpha()
 
-# -----------------Bgfunc---------------------------------------------------------------------------------------------------------------------------
+# Fonction pour dessiner le fond d'écran
 
 
 def draw_bg(bg_scroll, cycle):
-    # screen.blit(bg_image, (0,0+bg_scroll))
-    screen.blit(bg_image, (0, 0+bg_scroll))   # 912??
+    screen.blit(bg_image, (0, 0 + bg_scroll))
     for i in range(cycle):
-        screen.blit(bg2_image, (0, i*(-1824)-912+bg_scroll))
-        screen.blit(bg3_image, (0, i*(-1824)-1824+bg_scroll))
+        screen.blit(bg2_image, (0, i * (-1824) - 912 + bg_scroll))
+        screen.blit(bg3_image, (0, i * (-1824) - 1824 + bg_scroll))
 
+# Fonction pour afficher du texte à l'écran
 
-# -----------------Text output func---------------------------------------------------------------------------------------------------------------------------
 
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
 
-# -----------------Classes--------------------------------------------------------------------------------------------------------------------------
+# Classe Joueur
 
 
 class Player():
@@ -105,11 +85,11 @@ class Player():
 
         key = pygame.key.get_pressed()
 
-        if key[pygame.K_LEFT] == True:
+        if key[pygame.K_LEFT]:
             dx = -10
             self.flip = False
 
-        if key[pygame.K_RIGHT] == True:
+        if key[pygame.K_RIGHT]:
             dx = 10
             self.flip = True
 
@@ -117,24 +97,20 @@ class Player():
         dy += self.vel_y
 
         if self.rect.left + dx < 0:
-            # dx = -self.rect.left
             dx += 600
         if self.rect.right + dx > SCREEN_WIDTH:
-            # dx = SCREEN_WIDTH - self.rect.right
             dx -= 600
 
+        # Collision avec les plateformes
         for platform in platform_group:
-            if platform.rect.colliderect(self.rect.x, self.rect.y+dy, self.width, self.height):
-                # ахахаха чё я накодил. Все настройки через жопу !! centery можно на top поменять, но надо тестить стабильность
-                if self.rect.bottom < platform.rect.centery:
-                    if self.vel_y > 0:
-                        self.rect.bottom = platform.rect.top
-                        dy = 0
-                        self.vel_y = -18  # ахахаха чё я накодил. Все настройки через жопу 18
+            if platform.rect.colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                if self.rect.bottom < platform.rect.centery and self.vel_y > 0:
+                    self.rect.bottom = platform.rect.top
+                    dy = 0
+                    self.vel_y = -18
 
-        if self.rect.top <= SCROLL_THRESH:
-            if self.vel_y < 0:
-                scroll = -dy
+        if self.rect.top <= SCROLL_THRESH and self.vel_y < 0:
+            scroll = -dy
 
         self.rect.x += dx
         self.rect.y += dy + scroll
@@ -142,111 +118,81 @@ class Player():
         return scroll
 
     def draw(self):
-        if self.flip == False:
-            drect = 10
-        else:
-            drect = 8
+        screen.blit(pygame.transform.flip(self.image, self.flip, False),
+                    (self.rect.x - (10 if not self.flip else 8), self.rect.y - 20))
 
-        screen.blit(pygame.transform.flip(self.image, self.flip,
-                    False), (self.rect.x-drect, self.rect.y-20))
-        # pygame.draw.rect(screen,WHITE, self.rect,2)
+# Classe Plateforme
 
 
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, width):
-        pygame.sprite.Sprite.__init__(self)
+        super().__init__()
         self.image = pygame.transform.scale(platform_neon_image, (width, 10))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
     def update(self, scroll):
-        global score
-        global score2
-        global cycle
         self.rect.y += scroll
 
         if self.rect.top > SCREEN_HEIGHT:
             self.kill()
-            # score += 1
             score2 += 1
-            # print(score2) ---> на проверку
+
         if score2 >= 10:
             score2 = 0
             cycle += 1
-            # print(f'cycle :{cycle}') ---> на проверку
 
 
-Slime = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT-150)
-
+# Initialisation du joueur et des plateformes
+Slime = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
 platform_group = pygame.sprite.Group()
-
-platform = Platform(SCREEN_WIDTH // 2 - 45, SCREEN_HEIGHT-90, 80)
+platform = Platform(SCREEN_WIDTH // 2 - 45, SCREEN_HEIGHT - 90, 80)
 platform_group.add(platform)
-# -----------------Game----------------------------------------------------------------------------------------------------------------------------
 
+# Boucle de jeu principale
 run = True
-
-while run == True:
-
+while run:
     clock.tick(FPS)
 
-    if game_over == False:
-
+    if not game_over:
         scroll = Slime.move()
-
         bg_scroll += scroll
-
         draw_bg(bg_scroll, cycle)
 
-        print(bg_scroll)
-
+        # Génération des nouvelles plateformes
         if len(platform_group) < MAX_PLATFORMS:
             p_w = random.randint(60, 80)
-            p_x = random.randint(0, SCREEN_WIDTH - p_w - 40)
+            p_x = random.randint(0, SCREEN_WIDTH - p_w)
             p_y = platform.rect.y - random.randint(80, 120)
             platform = Platform(p_x, p_y, p_w)
             platform_group.add(platform)
 
-        # pygame.draw.line(screen,WHITE,(0,SCROLL_THRESH),(SCREEN_WIDTH ,SCROLL_THRESH)) #на проверочку
-
         platform_group.update(scroll)
-
         platform_group.draw(screen)
-
         Slime.draw()
 
         if Slime.rect.top > SCREEN_HEIGHT:
             game_over = True
             score = bg_scroll
-
     else:
-        draw_text('GAME OVER', font_big, WHITE,
-                  SCREEN_WIDTH // 2-70, SCREEN_HEIGHT // 2-100)
+        draw_text('GAME OVER', font_big, WHITE, SCREEN_WIDTH //
+                  2 - 70, SCREEN_HEIGHT // 2 - 100)
         draw_text('SCORE: ' + str(score), font_big, WHITE,
-                  SCREEN_WIDTH // 2-55, SCREEN_HEIGHT // 2-50)
+                  SCREEN_WIDTH // 2 - 55, SCREEN_HEIGHT // 2 - 50)
         draw_text('HIGH SCORE: ' + str(high_score), font_big, WHITE,
-                  SCREEN_WIDTH // 2-85, SCREEN_HEIGHT // 2-20)
+                  SCREEN_WIDTH // 2 - 85, SCREEN_HEIGHT // 2 - 20)
         draw_text('PRESS SPACE TO RESTART', font_big, WHITE,
-                  SCREEN_WIDTH // 2-150, SCREEN_HEIGHT // 2)
+                  SCREEN_WIDTH // 2 - 150, SCREEN_HEIGHT // 2)
         key = pygame.key.get_pressed()
-
-        # Update high score if new score is greater
-        if score > high_score:
-            high_score = score
-            with open("topscore.txt", "w") as score_file:
-                score_file.write(str(high_score))
-
         if key[pygame.K_SPACE]:
             game_over = False
             score = 0
-            scroll = 0
             bg_scroll = 0
             cycle = 1
-            Slime.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT-150)
+            Slime.rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT - 150)
             platform_group.empty()
-            # спавним платформу потому что она вне gameloop
-            platform = Platform(SCREEN_WIDTH // 2 - 45, SCREEN_HEIGHT-90, 80)
+            platform = Platform(SCREEN_WIDTH // 2 - 45, SCREEN_HEIGHT - 90, 80)
             platform_group.add(platform)
 
     for event in pygame.event.get():
@@ -254,6 +200,5 @@ while run == True:
             run = False
 
     pygame.display.update()
-
 
 pygame.quit()
---
+++ Doodlejump/README.md
@@ -0,0 +1,21 @@
+**Contribution de l'équipe au projet de jeu Pygame - Doodle Jump**
+
+Dans ce projet, chaque membre de l'équipe a apporté ses compétences et son expertise pour réaliser différentes parties du jeu.
+
+- **Artush** : Responsable de la génération des plateformes, de l'enregistrement du meilleur score et de contributions au design. Ces éléments ont été essentiels pour donner au jeu sa structure de base et son aspect visuel engageant.
+
+- **Alexandre** : A travaillé sur la logique de la gravitation et du saut, ainsi que sur la transition fluide du personnage d'un bord à l'autre de l'écran. Il a également correctement implémenté les images utilisées dans le jeu, garantissant que les aspects visuels fonctionnent bien avec la logique de jeu.
+
+- **Paul** : A développé le système de contrôle et a mis en place un système de score basé sur le défilement. Son travail a permis d'ajouter un élément compétitif au jeu, incitant les joueurs à améliorer continuellement leur performance.
+
+- **Noélie** : En tant que designer et chef de projet, Noélie a supervisé la conception globale et a aidé l'équipe à naviguer entre les différentes tâches. Elle a joué un rôle crucial dans la coordination de l'équipe et la définition de l'esthétique du jeu.
+
+- **Artush, Alexandre, Noélie, Paul** : Ensemble, ils ont travaillé à l'intégration des différentes parties du jeu, assurant une cohésion globale, la gestion des erreurs et le respect d'une structure de projet définie.
+
+**Difficultés rencontrées :**
+
+Le projet a présenté plusieurs défis, notamment les différences de connaissances en programmation, Noélie et Paul ayant dû apprendre en cours de projet pour suivre Artush et Alexandre. Comprendre la logique du jeu au début a été un défi, ainsi que la création d'une transition fluide entre deux images de fond. Centrer le texte sur l'écran final a également posé problème, et nous n'avons pas réussi à implémenter un écran de démarrage sans perturber la logique du jeu durant la semaine, d'autant plus que l'absence d'Alexandre a rendu la tâche plus ardue. L'implémentation de la logique pour un "super saut" a été particulièrement difficile, et nous n'avons pas réussi à le rendre fluide.
+
+**Améliorations envisagées :**
+
+Pour améliorer notre jeu, nous envisageons de structurer mieux le code, d'ajouter un menu principal expliquant les contrôles et d'introduire des ennemis pour dynamiser le jeu. Ces améliorations aideraient à rendre le jeu plus interactif et intéressant pour les joueurs.
